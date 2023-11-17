42c030ee9468b3ab29c4eade4329b18ce9a9e946
aalexebr <alex.ebr@icloud.com>
2023-11-17 11:58:54 +0100

    esercizio 1 no bonus complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
+//		System.out.println("test");
+		Scanner in = new Scanner(System.in);
+		
+		List <String> giftArray = new ArrayList <>();
+		
+		while(true) {
+			System.out.print("add item? y/n");
+			String next = in.nextLine();
+			char c = next.charAt(0);
+			if(c== 'n') break;
+			
+			System.out.print("add new item: ");
+			String gift = in.nextLine();
+			
+			giftArray.add(gift);
+			giftArray.size();
+			System.out.println("list length: "+giftArray.size());
+				
+			
+		}
+		System.out.println(giftArray);
+		in.close();
 	}
 
 }
