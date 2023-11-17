46dcf78695f8b6b912d9c16bae0a431e54962c8a
aalexebr <alex.ebr@icloud.com>
2023-11-17 13:53:51 +0100

    correction

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -43,12 +43,25 @@ public class Main {
 				
 			
 		}
+		
+		System.out.print("how to order? \n recepient:gift press 1 \n gift:recepient press 2");
+		String choice = in.nextLine();
+		for(String ch : giftMap.keySet()) {
+			String x = giftMap.get(ch);
+			if(choice.charAt(0)=='1') {
+				System.out.println(ch+" gets "+x);
+			}
+			else {
+				System.out.println(x+" for "+ch);
+			}
+			
+		}
 //		System.out.println(giftArray);
 		in.close();
 //		bonus print
 //		System.out.println(giftObjArray);
 //		bonus2 print
-		System.out.println(giftMap);
+//		System.out.println(giftMap);
 		
 		
 	}
